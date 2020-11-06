4a79ef5558810ffd9206b107d4263cd11cf16185
Alan <alanyny.yang@mail.utoronto.ca>
2020-11-06 10:57:14 -0500

    added function for plotting trace

--- R/visualization.R
+++ R/visualization.R
@@ -13,3 +13,23 @@ plot_te <- function(te) {
   # Histogram showing variability in individual total QALYs
   plot(density(te), main = paste("Total QALYs per person"), xlab = "QALYs")
 }
+
+# plot health state trace
+plot_m_TR <- function(m_M) {
+  # plot the distribution of the population across health states over time (trace)
+  # count the number of individuals in each health state at each cycle
+  m_TR <- t(apply(m_M, 2, function(x) table(factor(x, levels = v_n, ordered = TRUE)))) 
+  m_TR <- m_TR / n_i                                       # calculate the proportion of individuals 
+  colnames(m_TR) <- v_n                                    # name the rows of the matrix
+  rownames(m_TR) <- paste("Cycle", 0:n_t, sep = " ")       # name the columns of the matrix
+  # Plot trace of first health state
+  plot(0:n_t, m_TR[, 1], type = "l", main = "Health state trace", 
+       ylim = c(0, 1), ylab = "Proportion of cohort", xlab = "Cycle")
+  # add a line for each additional state
+  for (n_states in 2:length(v_n)) {
+    lines(0:n_t, m_TR[, n_states], col = n_states)  # adds a line to current plot
+  }
+  legend("topright", v_n, col = 1:4,    # add a legend to current plot
+         lty = rep(1, 3), bty = "n", cex = 0.65)
+  
+}
