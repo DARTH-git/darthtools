423b193806187f2ab7e1a0a36ac48288e93fd655
Alan <alanyny.yang@mail.utoronto.ca>
2020-11-09 23:15:44 -0500

    uploaded

--- R/visualization.R
+++ R/visualization.R
@@ -33,3 +33,20 @@ plot_m_TR <- function(m_M) {
          lty = rep(1, 3), bty = "n", cex = 0.65)
   
 }
+
+# plot health state trace
+plot_m_TR_shiny <- function(m_M, input_list = NULL) {
+  with(input_list,{
+    # plot the distribution of the population across health states over time (trace)
+    # count the number of individuals in each health state at each cycle
+    m_TR <- t(apply(m_M, 2, function(x) table(factor(x, levels = v_n, ordered = TRUE)))) 
+    m_TR <- m_TR / n_i                                       # calculate the proportion of individuals 
+    colnames(m_TR) <- v_n                                    # name the rows of the matrix
+    rownames(m_TR) <- paste("Cycle", 0:n_t, sep = " ")       # name the columns of the matrix
+    # Plot trace of first health state
+    matplot(m_TR, type = "l", main = "Health state trace", col= 1:n_states,
+            ylim = c(0, 1), ylab = "Proportion of cohort", xlab = "Cycle")
+    legend("topright", v_n, col = 1:n_states,    # add a legend to current plot
+           lty = rep(1, 3), bty = "n", cex = 0.65)
+  })
+}
