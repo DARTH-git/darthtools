18ebab565a72b5e2015b86010c5ccc2b5d8ff9ee
Alan <alanyny.yang@mail.utoronto.ca>
2020-11-13 14:40:08 -0500

    built packages

--- R/rate_conversion.R
+++ R/rate_conversion.R
@@ -1,6 +1,3 @@
-#----------------------------------------------------------------------------#
-####              Function to convert probabilities to rates              ####
-#----------------------------------------------------------------------------#
 #' Convert a probability to a rate
 #'
 #' \code{prob_to_rate} checks if a probability is between 0 and 1 and convert it to a rate.
@@ -8,7 +5,7 @@
 #' @param p probability
 #' @param t time/ frequency
 #' @return a number - converted rate
-#' 
+#' @export
 prob_to_rate <- function(p, t = 1){
   if (sum(p > 1) >0 | sum(p < 0) > 0 ){
     print("probability not between 0 and 1")
@@ -17,9 +14,6 @@ prob_to_rate <- function(p, t = 1){
   return(r)
 }
 
-#----------------------------------------------------------------------------#
-####              Function to convert rates to probabilities              ####
-#----------------------------------------------------------------------------#
 #' Convert a rate to a probability
 #'
 #' \code{rate_to_prob} convert a rate to a probability.
@@ -27,16 +21,12 @@ prob_to_rate <- function(p, t = 1){
 #' @param r rate
 #' @param t time/ frequency
 #' @return a number - converted probability
-#' 
-# Function to convert rates to probabilities
+#' @export
 rate_to_prob <- function(r, t = 1){
   p <- 1 - exp(- r * t)
   return(p)
 }
 
-#---------------------------------------------------------------------------------------------#
-#### Function to convert convert probabilities to probabilities with a different frequency ####
-#---------------------------------------------------------------------------------------------#
 #' Convert a probability to a probability with a different frequency
 #'
 #' \code{rate_to_prob} convert a probability to a probability with a different frequency.
@@ -44,9 +34,9 @@ rate_to_prob <- function(r, t = 1){
 #' @param p probability
 #' @param t time/ frequency
 #' @return a number - converted probability
-#' 
-# Function to convert probabilities to probabilities with a different frequency
+#' @export
 prob_to_prob <- function(p, t = 1){
   p_new <- RateProb(ProbRate(p, t))
   return(p_new)
 }
+
