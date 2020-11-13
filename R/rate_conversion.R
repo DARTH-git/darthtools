80906495d15d905611e941013ef7f96a4fef06aa
Alan <alanyny.yang@mail.utoronto.ca>
2020-11-13 11:21:38 -0500

    added prob_to_prob

--- R/rate_conversion.R
+++ R/rate_conversion.R
@@ -8,7 +8,7 @@
 #' @param p probability
 #' @param t time/ frequency
 #' @return a number - converted rate
-#'
+#' 
 prob_to_rate <- function(p, t = 1){
   if (sum(p > 1) >0 | sum(p < 0) > 0 ){
     print("probability not between 0 and 1")
@@ -27,9 +27,26 @@ prob_to_rate <- function(p, t = 1){
 #' @param r rate
 #' @param t time/ frequency
 #' @return a number - converted probability
-#'
+#' 
 # Function to convert rates to probabilities
 rate_to_prob <- function(r, t = 1){
   p <- 1 - exp(- r * t)
   return(p)
+}
+
+#---------------------------------------------------------------------------------------------#
+#### Function to convert convert probabilities to probabilities with a different frequency ####
+#---------------------------------------------------------------------------------------------#
+#' Convert a probability to a probability with a different frequency
+#'
+#' \code{rate_to_prob} convert a probability to a probability with a different frequency.
+#'
+#' @param p probability
+#' @param t time/ frequency
+#' @return a number - converted probability
+#' 
+# Function to convert probabilities to probabilities with a different frequency
+ProbProb <- function(p, t = 1){
+  p_new <- RateProb(ProbRate(p, t))
+  return(p_new)
 }
