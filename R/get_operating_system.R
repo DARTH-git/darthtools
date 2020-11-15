d9b8c5f4bd6245d12aab9b9a8d7a66dd3b0a9926
Fernando Alarid-Escudero <feralaes@users.noreply.github.com>
2020-11-15 17:31:28 -0600

    Create get_operating_system.R

---
+++ R/get_operating_system.R
@@ -0,0 +1,21 @@
+#' Get operating system
+#'
+#' \code{get_os} gets the operating system.
+#' @return
+#' A string with the operating system.
+#' @export
+get_os <- function(){
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)){
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "MacOSX"
+  } else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  tolower(os)
+}
