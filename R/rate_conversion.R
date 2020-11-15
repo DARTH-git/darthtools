02386d00755522000ba100ca704c3ee501b24449
Fernando Alarid-Escudero <feralaes@users.noreply.github.com>
2020-11-15 13:07:18 -0600

    update rate-prob converstion functions
    -Added examples
    - Improved error checking

--- R/rate_conversion.R
+++ R/rate_conversion.R
@@ -3,12 +3,17 @@
 #' \code{prob_to_rate} checks if a probability is between 0 and 1 and convert it to a rate.
 #'
 #' @param p probability
-#' @param t time/ frequency
-#' @return a number - converted rate
+#' @param t time/frequency
+#' @return a scalar or vector with rates
+#' @examples
+#' # Annual probability to monthly rate
+#' p_year  <- 0.3
+#' r_month <- prob_to_rate(p = p_year, t = 1/12)
+#' r_month
 #' @export
 prob_to_rate <- function(p, t = 1){
-  if (sum(p > 1) >0 | sum(p < 0) > 0 ){
-    print("probability not between 0 and 1")
+  if ((sum(p > 1) > 0) | (sum(p < 0) > 0)){
+    stop("probability not between 0 and 1")
   }
   r <- -log(1-p)/ t
   return(r)
@@ -20,9 +25,17 @@ prob_to_rate <- function(p, t = 1){
 #'
 #' @param r rate
 #' @param t time/ frequency
-#' @return a number - converted probability
+#' @return a scalar or vector with probabilities
+#' @examples
+#' # Annual rate to monthly probability
+#' r_year  <- 0.3
+#' r_month <- rate_to_prob(r = r_year, t = 1/12)
+#' r_month
 #' @export
 rate_to_prob <- function(r, t = 1){
+  if ((sum(r < 0) > 0)){
+    stop("rate not greater than or equal to 0")
+  }
   p <- 1 - exp(- r * t)
   return(p)
 }
@@ -33,9 +46,17 @@ rate_to_prob <- function(r, t = 1){
 #'
 #' @param p probability
 #' @param t time/ frequency
-#' @return a number - converted probability
+#' @return a scalar or vector of probabilities converted to a different frequency
+#' @examples
+#' # Annual probability to monthly probability
+#' p_year  <- 0.3
+#' p_month <- prob_to_prob(p = p_year, t = 1/12)
+#' p_month
 #' @export
 prob_to_prob <- function(p, t = 1){
+  if ((sum(p > 1) > 0) | (sum(p < 0) > 0)){
+    stop("probability not between 0 and 1")
+  }
   p_new <- 1-(1-p)^(t)
   return(p_new)
 }
