50657d0f0dca39c5e7ef58706600a91b9555af6e
Alan <alanyny.yang@mail.utoronto.ca>
2022-05-05 14:00:20 -0400

    samplev fernando new version slack vcat

--- R/samplev.R
+++ R/samplev.R
@@ -3,7 +3,7 @@
 #' \code{samplev} sample states for multiple individuals simultaneously.
 #'
 #' @param m_Probs matrix with probabilities (n_i * n_states)
-#' @param m number of cycles to sample
+#' @param m number of time cycles to sample
 #' @return n_i x 1 matrix filled with sampled health state(s) per individual
 #' @export
 samplev <- function(m_Probs, m = 1) {
@@ -13,11 +13,10 @@ samplev <- function(m_Probs, m = 1) {
   # Return
   # ran:    n.i x m matrix filled with sampled health state(s) per individual
   require(matrixStats)
+  lev <- dimnames(m_Probs)[[2]]  # extract the names of the health states considered for sampling
   n_samp <- nrow(m_Probs)
   u <- runif(n_samp, min = 0, max = 1)
-  v_sum_p <-rowCumsums(m_Probs)
-  v_cat <- max.col(v_sum_p >= u, ties.method = "first")
+  v_sum_p <- rowCumsums(m_Probs)
+  v_cat <- lev[max.col(v_sum_p >= u, ties.method = "first")]
   return(v_cat)
-} # close the function
-
-
+}
