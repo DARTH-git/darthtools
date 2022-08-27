9b3220669e0dbdf52c3e4964d72d44ac8738064c
Alan <alanyang0924@gmail.com>
2022-08-27 00:22:38 -0400

    added plot.icers

--- R/visualization.R
+++ R/visualization.R
@@ -372,3 +372,124 @@ update_param_list <- function(l_params_all, params_updated){
   l_params_all <- modifyList(l_params_all, params_updated) #update the values
   return(l_params_all)
 }
+
+#' Plot of ICERs
+#'
+#' Plots the cost-effectiveness plane for a ICER object, calculated with \code{\link{calculate_icers}}
+#' @param x Object of class \code{icers}.
+#' @inheritParams add_common_aes
+#' @param currency string. with currency used in the cost-effectiveness analysis (CEA).
+#' @param effect_units string. unit of effectiveness
+#' @param label whether to label strategies on the efficient frontier, all strategies, or none.
+#' defaults to frontier.
+#' @param label_max_char max number of characters to label the strategies - if not NULL (the default)
+#' longer strategies are truncated to save space.
+#' @param plot_frontier_only only plot the efficient frontier
+#' @param alpha opacity of points
+#' @inheritParams ggrepel::geom_label_repel
+#'
+#' @return a ggplot2 object which can be modified by adding additional geoms
+#'
+#' @importFrom stringr str_sub
+#' @importFrom ggrepel geom_label_repel
+#' @export
+plot.icers <- function(x,
+                       txtsize = 12,
+                       currency = "$",
+                       effect_units = "QALYs",
+                       label = c("frontier", "all", "none"),
+                       label_max_char = NULL,
+                       plot_frontier_only = FALSE,
+                       alpha = 1,
+                       n_x_ticks = 6,
+                       n_y_ticks = 6,
+                       xbreaks = NULL,
+                       ybreaks = NULL,
+                       xlim = NULL,
+                       ylim = NULL,
+                       xexpand = expansion(0.1),
+                       yexpand = expansion(0.1),
+                       max.iter = 20000,
+                       ...) {
+  if (ncol(x) > 7) {
+    # reformat icers class object if uncertainty bounds are present
+    x <- x %>%
+      select(.data$Strategy, .data$Cost, .data$Effect,
+             .data$Inc_Cost, .data$Inc_Effect,
+             .data$ICER, .data$Status)
+  }
+
+  # type checking
+  label <- match.arg(label)
+
+  # this is so non-dominated strategies are plotted last (on top)
+  x <- arrange(x, .data$Status)
+
+  # change status text in data frame for plotting
+  d_name <- "Dominated"
+  ed_name <- "Weakly Dominated"
+  nd_name <- "Efficient Frontier"
+
+  status_expand <- c("D" = d_name, "ED" = ed_name,
+                     "ND" = nd_name, "ref" = nd_name)
+  x$Status <- factor(status_expand[x$Status], ordered = FALSE,
+                     levels = c(d_name, ed_name, nd_name))
+
+  # linetype
+  plot_lines <- c("Dominated" = "blank",
+                  "Weakly Dominated" = "blank",
+                  "Efficient Frontier" = "solid")
+
+  # names to refer to in aes_
+  stat_name <- "Status"
+  strat_name <- "Strategy"
+  eff_name <- "Effect"
+  cost_name <- "Cost"
+
+  # frontier only
+  if (plot_frontier_only) {
+    plt_data <- x[x$Status == nd_name, ]
+  } else {
+    plt_data <- x
+  }
+
+  # make plot
+  icer_plot <- ggplot(plt_data, aes_(x = as.name(eff_name), y = as.name(cost_name),
+                                     shape = as.name(stat_name))) +
+    geom_point(alpha = alpha, size = 2) +
+    geom_line(aes_(linetype = as.name(stat_name), group = as.name(stat_name))) +
+    scale_linetype_manual(name = NULL, values = plot_lines) +
+    scale_shape_discrete(name = NULL) +
+    labs(x = paste0("Effect (", effect_units, ")"),
+         y = paste0("Cost (", currency, ")"))
+
+  icer_plot <- add_common_aes(icer_plot, txtsize, col = "none",
+                              continuous = c("x", "y"),
+                              n_x_ticks = n_x_ticks, n_y_ticks = n_y_ticks,
+                              xbreaks = xbreaks, ybreaks = ybreaks,
+                              xlim = xlim, ylim = ylim,
+                              xexpand = xexpand, yexpand = yexpand)
+
+  # labeling
+  if (label != "none") {
+    if (!is.null(label_max_char)) {
+      plt_data[, strat_name] <- str_sub(plt_data[, strat_name],
+                                        start = 1L, end = label_max_char)
+    }
+    if (label == "all") {
+      lab_data <- plt_data
+    }
+    if (label == "frontier") {
+      lab_data <- plt_data[plt_data$Status == nd_name, ]
+    }
+
+    icer_plot <- icer_plot +
+      ggrepel::geom_label_repel(data = lab_data,
+                                aes_(label = as.name(strat_name)),
+                                size = 3,
+                                show.legend = FALSE,
+                                max.iter = max.iter,
+                                direction = "both")
+  }
+  return(icer_plot)
+}
