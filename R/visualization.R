817601c0b88ee47ae4ab121d6f434e97dbfe0bf2
Alan <alanyang0924@gmail.com>
2023-11-07 16:26:15 -0500

    Update visualization.R

--- R/visualization.R
+++ R/visualization.R
@@ -33,7 +33,8 @@ plot_trace_microsim <- function(m_M) {
   # plot the distribution of the population across health states over time (trace)
   # count the number of individuals in each health state at each cycle
   m_TR <- t(apply(m_M, 2, function(x) table(factor(x, levels = v_names_states, ordered = TRUE))))
-  m_TR <- m_TR / n_i                                 # calculate the proportion of individuals
+  # m_TR <- m_TR / n_i                                 # calculate the proportion of individuals
+  m_TR <- m_TR / nrow(m_TR)
   colnames(m_TR) <- v_names_states                   # name the rows of the matrix
   rownames(m_TR) <- paste("Cycle", 0:(ncol(m_M)-1), sep = " ") # name the columns of the matrix
   # Plot trace of first health state
