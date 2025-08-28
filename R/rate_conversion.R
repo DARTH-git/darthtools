0a1b5fc20b48d1f03451dace8e43cd6087e72f09
Qiyue <105830788+Lilly-20@users.noreply.github.com>
2025-08-27 20:24:07 -0400

    fix Probability/rate conversion functions

--- R/rate_conversion.R
+++ R/rate_conversion.R
@@ -24,40 +24,46 @@ prob_to_rate <- function(p, t = 1){
 #' \code{rate_to_prob} convert a rate to a probability.
 #'
 #' @param r rate
-#' @param t time/ frequency
+#' @param t number of cycles per base time unit (frequency)
 #' @return a scalar or vector with probabilities
 #' @examples
 #' # Annual rate to monthly probability
 #' r_year  <- 0.3
-#' r_month <- rate_to_prob(r = r_year, t = 1/12)
+#' r_month <- rate_to_prob(r = r_year, t = 12)
 #' r_month
 #' @export
 rate_to_prob <- function(r, t = 1){
-  if ((sum(r < 0) > 0)){
-    stop("rate not greater than or equal to 0")
+  if (any(r < 0, na.rm = TRUE)){
+    stop("`r` must be >= 0.")
   }
-  p <- 1 - exp(- r * t)
+  if (any(t <= 0, na.rm = TRUE)){
+    stop("`t` must be > 0.")
+  }
+  p <- 1 - exp(- r / t)
   return(p)
 }
 
 #' Convert a probability to a probability with a different frequency
 #'
-#' \code{rate_to_prob} convert a probability to a probability with a different frequency.
+#' \code{prob_to_prob} convert a probability to a probability with a different frequency.
 #'
 #' @param p probability
-#' @param t time/ frequency
+#' @param t number of cycles per base time unit (frequency)
 #' @return a scalar or vector of probabilities converted to a different frequency
 #' @examples
 #' # Annual probability to monthly probability
 #' p_year  <- 0.3
-#' p_month <- prob_to_prob(p = p_year, t = 1/12)
+#' p_month <- prob_to_prob(p = p_year, t = 12)
 #' p_month
 #' @export
 prob_to_prob <- function(p, t = 1){
-  if ((sum(p > 1) > 0) | (sum(p < 0) > 0)){
-    stop("probability not between 0 and 1")
+  if (any(p < 0 | p > 1, na.rm = TRUE)) {
+    stop("`p` must be between 0 and 1.")
   }
-  p_new <- 1-(1-p)^(t)
+  if (any(t <= 0, na.rm = TRUE)) {
+    stop("`t` must be > 0.")
+  }
+  p_new <- 1-(1-p)^(1/t)
   return(p_new)
 }
 
